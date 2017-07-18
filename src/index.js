15add2e1469fa86dac530e4919c0990ed21b7328
Binh-Phi Nguyen <kugoo109@gmail.com>
2017-07-18 12:39:33 +0200

    config webpack to seperate vendor, app and css into different file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-/*eslint-disable */
-import SC from 'soundcloud';
-/*eslint-enable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './stores/configureStore';
 import App from './components/App';
-
-require('../styles/index.scss');
 
 const store = configureStore();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,80 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.js'
-  ],
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
+  entry: {
+    vendor: [
+      'bootstrap',
+      'classnames',
+      'jquery',
+      'js-cookie',
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'react-redux',
+      'redux',
+      'redux-thunk',
+      'lodash',
+      'prop-types',
     ],
+    app: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './src/index.js'
+    ],
+    'vendor-style': [
+      './node_modules/animate.css/animate.css',
+      './node_modules/bootstrap/dist/css/bootstrap.css',
+      './node_modules/font-awesome/css/font-awesome.css',
+    ],
+    style: './styles/index.scss',
+  },
+  module: {
     loaders: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: "babel-loader"
     },
     {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
+      test: /\.(css|scss)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: "css-loader!sass-loader",
+      })
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, '/dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'js/[name].js'
   },
   devServer: {
     contentBase: './dist',
     hot: true,
     historyApiFallback: true
   },
-  eslint: {
-    configFile: './.eslintrc'
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({ name: ['app', 'vendor'], minChunks: Infinity }),
+    new ExtractTextPlugin({ filename: 'css/[name].css', allChunks: true }),
     new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.ProvidePlugin({
+      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': '"development"'
+        NODE_ENV: '"development"'
       }
     })
   ]
